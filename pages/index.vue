1c27f61b607ee178d939af0b17e74c103c392802
yangwenjie <yangwenjie@zhifei-group.com>
2022-02-14 17:11:24 +0800

    去掉部分注释

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
-const count = ref(0);
-const config = useRuntimeConfig();
-console.log(config);
+// const count = ref(0);
+// const config = useRuntimeConfig();
+// console.log(config);
 </script>
