440976b08245a07d0cd940b1f7bb348094ba51a7
yangwenjie <yangwenjie@zhifei-group.com>
2022-03-22 09:39:34 +0800

    删除 文件

--- .nuxt/types/schema.d.ts
+++
@@ -1,6 +0,0 @@
-import { NuxtModule } from '@nuxt/schema'
-declare module '@nuxt/schema' {
-  interface NuxtConfig {
-    ["tailwindcss"]?: typeof import("@nuxtjs/tailwindcss").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-  }
-}
